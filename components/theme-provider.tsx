912895a8abdf0224e7a6c55ce1b30f7e98e82d36
codescalper <msfunbook@gmail.com>
2023-09-29 20:50:25 +0530

    Fixing focus on elemt issue

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,12 +1,9 @@
 "use client"
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
 
-import { ReactNode } from 'react';
-export function ThemeProvider({ children }: { children: ReactNode }) {
-    return (
-        <NextThemesProvider>
-            {children}
-        </NextThemesProvider>
-    )
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
