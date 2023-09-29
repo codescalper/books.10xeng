7d3410f319cc51cc1fd8150b2e8bcd39c8181776
codescalper <msfunbook@gmail.com>
2023-09-29 20:47:57 +0530

    Fixing theme-provider issue

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,10 +5,7 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ReactNode } from 'react';
 export function ThemeProvider({ children }: { children: ReactNode }) {
     return (
-        <NextThemesProvider
-            defaultTheme='dark' // Set default theme 
-            attribute='class'
-        >
+        <NextThemesProvider>
             {children}
         </NextThemesProvider>
     )
